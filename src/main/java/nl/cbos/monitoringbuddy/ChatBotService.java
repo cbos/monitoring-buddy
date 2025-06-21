709ef39effe5701773487a69cae0d2a7969bed06
Cees Bos <dev@ceesbos.nl>
2025-06-21 18:03:57 +0200

    Add Grafana MCP and more logging and tracing

--- src/main/java/nl/cbos/monitoringbuddy/ChatBotService.java
+++ src/main/java/nl/cbos/monitoringbuddy/ChatBotService.java
@@ -1,6 +1,8 @@
 package nl.cbos.monitoringbuddy;
 
+import dev.langchain4j.service.SystemMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
+import io.quarkiverse.langchain4j.mcp.runtime.McpToolBox;
 import io.smallrye.mutiny.Multi;
 import jakarta.enterprise.context.SessionScoped;
 
@@ -8,5 +10,20 @@ import jakarta.enterprise.context.SessionScoped;
 @RegisterAiService
 public interface ChatBotService {
 
+    @SystemMessage("""
+            You are a monitoring buddy who can help to identify problems.
+            You are friendly, polite and concise.
+            You can answer questions about the monitoring system, and you can help to identify problems.
+            
+            You know about the following tools:
+            Grafana, Loki, Prometheus, Tempo.
+            You should use these tools to answer questions.
+            Grafana MCP tool can be used to query Grafana dashboards and query prometheus and loki.
+            The Grafana server is available at http://localhost:3000 for the user, you can include that in the urls provided to the user.
+            Be eager to suggest relevant dashboards or queries that can help the user to identify problems, especially 'explore' urls that can be used to explore the data in Grafana.
+            
+            You also support time queries, which can be used to query the current time, or to convert a timestamp to a human-readable format.
+            """)
+    @McpToolBox({"grafana-stdio", "time"})
     Multi<String> chat(String userMessage);
 }
