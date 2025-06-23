1a3980503910807563f522bc1918cd48a6d756f9
Cees Bos <dev@ceesbos.nl>
2025-06-23 18:37:24 +0200

    Extend @SystemMessage to give some more guidance

--- src/main/java/nl/cbos/monitoringbuddy/ChatBotService.java
+++ src/main/java/nl/cbos/monitoringbuddy/ChatBotService.java
@@ -20,7 +20,24 @@ public interface ChatBotService {
             You should use these tools to answer questions.
             Grafana MCP tool can be used to query Grafana dashboards and query prometheus and loki.
             The Grafana server is available at http://localhost:3000 for the user, you can include that in the urls provided to the user.
-            Be eager to suggest relevant dashboards or queries that can help the user to identify problems, especially 'explore' urls that can be used to explore the data in Grafana.
+            
+            Service graph and span metrics are available.
+            With service graph metrics you can identify the relationships between services and if a service returned failures.
+            The 'client' attribute indicates the service that made the request, and the 'server' attribute indicates the service that received the request.
+            
+            | Metric                                      | Type      | Labels                          | Description                                                               |
+            |---------------------------------------------|-----------|----------------|---------------------------------------------------------------------------|
+            | traces_service_graph_request_total          | Counter   | client, server | Total count of requests between two nodes                                 |
+            | traces_service_graph_request_failed_total   | Counter   | client, server | Total count of failed requests between two nodes                          |
+            | traces_service_graph_request_server         | Histogram | client, server | Number of seconds for a request between two nodes as seen from the server |
+            | traces_service_graph_request_client         | Histogram | client, server | Number of seconds for a request between two nodes as seen from the client |
+            | traces_service_graph_unpaired_spans_total   | Counter   | client, server | Total count of unpaired spans                                             |
+            | traces_service_graph_dropped_spans_total    | Counter   | client, server | Total count of dropped spans                                              |
+            
+            Requests are handled in a chain of services. In case of a service fails, that has impact on the other services in the chain.
+            With the service graph can help to identify the problematic service.
+            
+            If available you can suggest relevant dashboards or queries that can help the user to identify problems, especially 'explore' urls that can be used to explore the data in Grafana.
             
             You also support time queries, which can be used to query the current time, or to convert a timestamp to a human-readable format.
             """)
